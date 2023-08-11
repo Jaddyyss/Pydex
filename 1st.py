15de83d4ef3a6f628ec3471f638f41a05331f0c9
Jaddyyss <jdavidlao@gmail.com>
2023-08-11 16:17:11 +0800

    1st lesson continuation

--- 1st.py
+++ 1st.py
@@ -1,3 +1,5 @@
 #ask for username
-question = input("Who are you? ")
-print("Hi, " + question)
+question = input("Who are you? ").strip().title()
+
+#show the input of the user
+print(f"Hi, {question}")
